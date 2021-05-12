1b5f9d41f8a7282e72b388e154366fd620c4ab4e
iykeafrica <ikechiucheagwu@gmail.com>
2021-05-12 02:59:49 +0100

    Error handling and general house keeping of code

--- src/main/java/com/iykeafrica/echange/ui/model/request/UserSendMoneyRequest.java
+++ src/main/java/com/iykeafrica/echange/ui/model/request/UserSendMoneyRequest.java
@@ -1,26 +1,8 @@
 package com.iykeafrica.echange.ui.model.request;
 
 public class UserSendMoneyRequest {
-    private String walletId;
-    private String requesterWalletId;
     private double lastSentReceivedAmount;
     private String transactionPin;
-
-    public String getWalletId() {
-        return walletId;
-    }
-
-    public void setWalletId(String walletId) {
-        this.walletId = walletId;
-    }
-
-    public String getRequesterWalletId() {
-        return requesterWalletId;
-    }
-
-    public void setRequesterWalletId(String requesterWalletId) {
-        this.requesterWalletId = requesterWalletId;
-    }
 
     public double getLastSentReceivedAmount() {
         return lastSentReceivedAmount;
